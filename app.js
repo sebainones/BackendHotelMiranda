6bf2b1f6b03f01ad7522186bbbb38d608f4f1d7a
Sebastian Inones <seba.inones@gmail.com>
2022-04-23 19:58:00 +0200

    Added support for CORS for Dev/Testing purposes!

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 var app = express();
 
+//Added support for CORS for Dev/Testing purposes!
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+ });
+
 const portNumber = process.env.PORT || 4041;
 
 app.get("/", (_, res, next) => {
@@ -9,6 +15,10 @@ app.get("/", (_, res, next) => {
 
 app.get("/names", (_, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food", "Sebas","Tian"]);
+});
+
+app.get("/courses", (_, res, next) => {
+    res.json(["Course API", "Course B", "Course C", "Course D"]);
 });
 
 app.listen(portNumber, () => {
