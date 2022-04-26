d26f1b670e030a57da6f04d7d700af61b3c7a493
Sebastian Inones <seba.inones@gmail.com>
2022-04-26 09:00:33 +0200

    Commented some lines just in case

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express";
 var app = express();
 
 //Added support for CORS for Dev/Testing purposes!
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
- });
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     next();
+//  });
 
 const portNumber = process.env.PORT || 4041;
 
@@ -17,9 +17,9 @@ app.get("/names", (_, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food", "Sebas","Tian"]);
 });
 
-app.get("/courses", (_, res, next) => {
-    res.json(["Course API", "Course B", "Course C", "Course D"]);
-});
+// app.get("/courses", (_, res, next) => {
+//     res.json(["Course API", "Course B", "Course C", "Course D"]);
+// });
 
 app.listen(portNumber, () => {
     console.log(`Server running on port: ${portNumber}` );
